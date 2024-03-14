0fd261acfc84fb4844b8abd36ccc2d5a3131a170
Jiaqi <2313737480@qq.com>
2024-03-14 14:45:37 +0800

    data

--- code/Bezier.py
+++ code/Bezier.py
@@ -20,28 +20,35 @@ path_x = []
 path_y = []
 points = np.arange(0 , len(path) , 10)
 for i in tqdm(range(1 , len(points))):
-    for t in np.arange(0,1,0.001):
+    for t in np.arange(0,1,0.005):
         pos = bezier(np.array(path[points[i - 1] : points[i]]) , len(path[points[i - 1] : points[i]]) , t)
-        path_x.append(pos[0])
-        path_y.append(pos[1])
+        path_x.append(np.round(pos[0] , 2))
+        path_y.append(np.round(pos[1] , 2))
 
 data = pd.read_excel("../data/map.xlsx" , header = None)
 data = data.to_numpy()
-path_y = [len(data[0]) - point for point in path_y]
+# path_y = [len(data[0]) - point for point in path_y]
 
-start = (282,377)
-goal = (0,25)
-plt.plot(path_y , path_x)
-plt.gca().invert_yaxis()
-plt.gca().invert_xaxis()
-plt.gca().xaxis.set_ticklabels([])
-plt.gca().yaxis.set_ticklabels([])
-plt.scatter(len(data[0]) - start[1],start[0],color = "red" ,s = 200)
-plt.scatter(len(data[0]) - goal[1],goal[0],color = "green" , s = 200)
-# plt.savefig("../image/bezier_path.png")
-plt.show()
+# start = (282,377)
+# goal = (0,25)
+# plt.plot(path_y , path_x)
+# plt.gca().invert_yaxis()
+# plt.gca().invert_xaxis()
+# plt.gca().xaxis.set_ticklabels([])
+# plt.gca().yaxis.set_ticklabels([])
+# plt.scatter(len(data[0]) - start[1],start[0],color = "red" ,s = 200)
+# plt.scatter(len(data[0]) - goal[1],goal[0],color = "green" , s = 200)
+# # plt.savefig("../image/bezier_path.png")
+# plt.show()
+
+
 # with open("../data/bezier_path.txt" , 'w') as file:
 #     for i in range(len(path_x)):
 #         file.write(f"{np.round(path_x[i],2)} , {np.round(path_y[i],2)}\n")
 
-
+path_new = []
+path_new.append(path_x)
+path_new.append(path_y)
+df = pd.DataFrame(path_new)
+df = df.T
+df.to_csv("../data/path_T.csv" , header=None , index=None)
